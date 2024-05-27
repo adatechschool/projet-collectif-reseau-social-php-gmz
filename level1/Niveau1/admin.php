8a9fccfac22746835893ac9ae95054c62f4673a7
Z0uLf4T <A.Zoulfat@gmail.com>
2024-05-27 18:29:53 +0200

    correction du fichier admin.php

--- level1/Niveau1/admin.php
+++ level1/Niveau1/admin.php
@@ -34,11 +34,11 @@
         //$mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
         include 'sqlConnection.php';
         //verification
-        // if ($mysqli->connect_errno)
-        // {
-        //     echo("Échec de la connexion : " . $mysqli->connect_error);
-        //     exit();
-        // }
+         if ($mysqli->connect_errno)
+         {
+             echo("Échec de la connexion : " . $mysqli->connect_error);
+             exit();
+         }
         ?>
         <div id="wrapper" class='admin'>
             <aside>
@@ -62,13 +62,13 @@
                  */
                 while ($tag = $lesInformations->fetch_assoc())
                 {
-                    echo "<pre>" . print_r($tag, 1) . "</pre>";
+                    
                     ?>
                     <article>
-                        <h3>#chaussette</h3>
-                        <p>id:321</p>
+                        <h3>#<?php echo $tag['label'] ?></h3>
+                        <p>id:<?php echo $tag['id'] ?></p>
                         <nav>
-                            <a href="tags.php?tag_id=321">Messages</a>
+                            <a href="tags.php?tag_id=<?php echo $tag['label'] ?>">Messages</a>
                         </nav>
                     </article>
                 <?php } ?>
@@ -95,17 +95,17 @@
                  */
                 while ($tag = $lesInformations->fetch_assoc())
                 {
-                    echo "<pre>" . print_r($tag, 1) . "</pre>";
+                    
                     ?>
                     <article>
-                        <h3>Alexandra</h3>
-                        <p>id:123</p>
+                        <h3><?php echo $tag['alias'] ?></h3>
+                        <p>id:<?php echo $tag['id'] ?></p>
                         <nav>
-                            <a href="wall.php?user_id=123">Mur</a>
-                            | <a href="feed.php?user_id=123">Flux</a>
-                            | <a href="settings.php?user_id=123">Paramètres</a>
-                            | <a href="followers.php?user_id=123">Suiveurs</a>
-                            | <a href="subscriptions.php?user_id=123">Abonnements</a>
+                            <a href="wall.php?user_id=<?php echo $tag['id'] ?>">Mur</a>
+                            | <a href="feed.php?user_id=<?php echo $tag['id'] ?>123">Flux</a>
+                            | <a href="settings.php?user_id=<?php echo $tag['id'] ?>">Paramètres</a>
+                            | <a href="followers.php?user_id=<?php echo $tag['id'] ?>">Suiveurs</a>
+                            | <a href="subscriptions.php?user_id=<?php echo $tag['id'] ?>">Abonnements</a>
                         </nav>
                     </article>
                 <?php } ?>
