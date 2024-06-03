764af4538342755120a50dff675fbacd4e502cb9
EliDevToBe <maxime.ktr@proton.me>
2024-06-03 12:25:13 +0200

    redirections sur page feed.php OK

--- level1/Niveau1/feed.php
+++ level1/Niveau1/feed.php
@@ -23,16 +23,16 @@ if (!$sessionId) {
         <img src="resoc.jpg" alt="Logo de notre réseau social" />
         <nav id="menu">
             <a href="news.php">Actualités</a>
-            <a href="wall.php?user_id=5">Mur</a>
-            <a href="feed.php?user_id=5">Flux</a>
+            <a href="wall.php">Mur</a>
+            <a href="feed.php?user_id=<?php echo $sessionId ?>">Flux</a>
             <a href="tags.php?tag_id=1">Mots-clés</a>
         </nav>
         <nav id="user">
             <a href="#">Profil</a>
             <ul>
-                <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
+                <li><a href="settings.php?user_id=<?php echo $sessionId ?>">Paramètres</a></li>
+                <li><a href="followers.php?user_id=<?php echo $sessionId ?>">Mes suiveurs</a></li>
+                <li><a href="subscriptions.php?user_id=<?php echo $sessionId ?>">Mes abonnements</a></li>
             </ul>
 
         </nav>
@@ -48,6 +48,9 @@ if (!$sessionId) {
          * Etape 1: Le mur concerne un utilisateur en particulier
          */
         $userId = intval($_GET['user_id']);
+        if (!$userId || $userId != $sessionId) {
+            Header("Location: ./feed.php?user_id=$sessionId");
+        }
         ?>
         <?php
         /**
