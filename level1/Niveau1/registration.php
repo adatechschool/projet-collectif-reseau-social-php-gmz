56ece67228754773a895d69c521a4b13d0f5ec6e
EliDevToBe <maxime.ktr@proton.me>
2024-06-03 16:29:17 +0200

    deleted unset() de la session dans la page registration

--- level1/Niveau1/registration.php
+++ level1/Niveau1/registration.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-unset($_SESSION["connected_id"]);
+// unset($_SESSION["connected_id"]);
 echo "<pre>" . print_r($_SESSION, 1) . "</pre>";
 
 if (isset($_SESSION["connected_id"])) {
