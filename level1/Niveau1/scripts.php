64db0476c78e019b7b101b8f4aa0fa6fde89ccd3
EliDevToBe <maxime.ktr@proton.me>
2024-06-05 15:15:20 +0200

    fix avec regex du detectTags avec les retour à la ligne et quelques char spéciaux

--- level1/Niveau1/scripts.php
+++ level1/Niveau1/scripts.php
@@ -1,60 +1,31 @@
 <?php
 
-function displayTags($string)
-{
-    include "./sqlConnection.php";
-
-    $allTagsQuery =
-        "
-        SELECT posts.content,
-            posts.created,
-            users.id,
-            users.alias as author_name,  
-            count(likes.id) as like_number,  
-            GROUP_CONCAT(DISTINCT tags.id) AS tagidlist,
-            GROUP_CONCAT(DISTINCT tags.label) AS taglist 
-        FROM posts
-        JOIN users ON  users.id=posts.user_id
-        LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
-        LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
-        LEFT JOIN likes      ON likes.post_id  = posts.id 
-        GROUP BY posts.id
-        ORDER BY posts.created DESC  
-        LIMIT 5
-        ";
-    $data = $mysqli->query($allTagsQuery);
-
-    if (count($taglist) > 1) {
-        foreach ($taglist as $tag) { ?>
-            <a href="./tags.php?tag_id=<?php echo $post['tagidlist'] ?>">#
-                <?php
-                $hashtag = str_replace(',', ', #', $post['taglist']);
-                echo $hashtag  ?></a>
-<?php
-        }
-    }
-}
-
 function detectTags($thePostContent, $thePostId)
 {
     include "./sqlConnection.php";
 
-    // ==== Delete les " ' "
-    $newString = explode("'", $thePostContent);
-    $stringAgain = implode(" ", $newString);
+    // // ==== Delete les " ' "
+    // $newString = explode("'", $thePostContent);
+    // $stringAgain = implode(" ", $newString);
 
-    // ==== Delete les " , "
-    $newString = explode(",", $stringAgain);
-    $stringAgain = implode(" ", $newString);
+    // // ==== Delete les " , "
+    // $newString = explode(",", $stringAgain);
+
+    $hastagRegex = "/[:.,'\r\n]/";
+    $newString = str_replace('\r\n', ' ', $thePostContent);
+    $stringAgain = preg_replace($hastagRegex, " ", $newString);
 
     $newStringArray = explode(" ", $stringAgain);
 
     // === Array qui va contenir les mots avec #
     foreach ($newStringArray as $word) {
         if (str_contains($word, "#")) {
-            $word = explode("#", $word);
-            $word = implode("", $word);
+
+            $hastagRegex = "/[ #!?\/\$*§%\^_]/";
+            $word = preg_replace($hastagRegex, "", $word);
             $word = trim($word);
+            // $word = explode("#", $word);
+            // $word = implode("", $word);
 
             $queryTagId = "
             SELECT id FROM tags WHERE tags.label='$word';
